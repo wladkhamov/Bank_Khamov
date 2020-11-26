6a34672ed71367f1a457985dc29d21b836f52bc7
Khamov Wlad <Wlad.hamov14@mail.ru>
2020-11-26 14:51:07 +0700

    Bank_Khamov

---
+++ app/src/main/java/com/example/ws_bank_bakunov/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.ws_bank_bakunov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public ImageView textalert;
+    public ImageButton btn;
+    public ImageButton apm;
+    final Context context = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageButton apm = findViewById(R.id.apm);
+        apm.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+//Инициализируем элементы:
+        textalert = (ImageView) findViewById(R.id.textAlert);
+        btn = (ImageButton) findViewById(R.id.btn_login);
+        //Добавляем слушателя нажатий по кнопке Button:
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                //Получаем вид с файла prompt.xml, который применим для диалогового окна:
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.login_alert, null);
+
+                //Создаем AlertDialog
+                AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(context);
+
+                //Настраиваем prompt.xml для нашего AlertDialog:
+                mDialogBuilder.setView(promptsView);
+
+                //Настраиваем сообщение в диалоговом окне:
+                mDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("Войти",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                        openActivity2();
+                                    }
+                                })
+                        .setNegativeButton("Отмена",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                //Создаем AlertDialog:
+                AlertDialog alertDialog = mDialogBuilder.create();
+                //и отображаем его:
+                alertDialog.show();
+            }
+        });
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+}
